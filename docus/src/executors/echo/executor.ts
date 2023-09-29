c1029c7c8f7e8ce8c5e799fa376e223dd645511c
Jeric Bas <jericrayebas@gmail.com>
2023-09-29 21:48:27 +0800

    chore: update executor

--- docus/src/executors/echo/executor.ts
+++ docus/src/executors/echo/executor.ts
@@ -1,8 +1,24 @@
-import { EchoExecutorSchema } from './schema';
+import type { ExecutorContext } from '@nx/devkit';
+import { exec } from 'child_process';
+import { promisify } from 'util';
 
-export default async function runExecutor(options: EchoExecutorSchema) {
-  console.log('Executor ran for Echo', options);
-  return {
-    success: true,
-  };
+export interface EchoExecutorOptions {
+  textToEcho: string;
+}
+
+export default async function echoExecutor(
+  options: EchoExecutorOptions,
+  context: ExecutorContext
+): Promise<{ success: boolean }> {
+  console.info(`Executing "echo"...`);
+  console.info(`Options: ${JSON.stringify(options, null, 2)}`);
+
+  const { stdout, stderr } = await promisify(exec)(
+    `echo ${options.textToEcho}`
+  );
+  console.log(stdout);
+  console.error(stderr);
+
+  const success = !stderr;
+  return { success };
 }
