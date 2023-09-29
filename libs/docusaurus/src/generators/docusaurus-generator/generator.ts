d9c15f63bbf661f33ea080bdd9c76ff28f4de468
Jeric Bas <jericrayebas@gmail.com>
2023-09-29 19:01:53 +0800

    chore: update generator

--- libs/docusaurus/src/generators/docusaurus-generator/generator.ts
+++ libs/docusaurus/src/generators/docusaurus-generator/generator.ts
@@ -13,30 +13,37 @@ export async function docusaurusGeneratorGenerator(
   options: DocusaurusGeneratorGeneratorSchema
 ) {
   const projectRoot = `apps/${options.name}`;
-  
-  // Create a new Docusaurus site with TypeScript using create-docusaurus
-  execSync(`npx create-docusaurus@latest ${projectRoot} classic --typescript`, { stdio: 'inherit' });
 
-  // Generate project.json
+  // Check if the project directory already exists
+  if (tree.exists(`${projectRoot}/package.json`)) {
+    console.error(`Cannot create a new project ${options.name} at ${projectRoot}. It already exists.`);
+  } else {
+    // Create a new Docusaurus site with TypeScript using create-docusaurus
+    execSync(`npx create-docusaurus@latest ${projectRoot} classic --typescript`, { stdio: 'inherit' });
+
+    // Add Nx project configuration
+    addProjectConfiguration(tree, options.name, {
+      root: projectRoot,
+      projectType: 'application',
+      sourceRoot: `${projectRoot}/src`,
+      targets: {
+        // Add your Nx targets here, if any
+      },
+    });
+  }
+
+  // Generate project.json regardless of whether the project directory existed
   const projectJsonPath = `${projectRoot}/project.json`;
-  const projectJsonContent = {
-    "scripts": {
-      "start": "docusaurus start",
-      "build": "docusaurus build"
-    },
-    // Add other custom configurations here
-  };
-  tree.write(projectJsonPath, JSON.stringify(projectJsonContent, null, 2));
-
-  // Add Nx project configuration
-  addProjectConfiguration(tree, options.name, {
-    root: projectRoot,
-    projectType: 'application',
-    sourceRoot: `${projectRoot}/src`,
-    targets: {
-      // Add your Nx targets here, if any
-    },
-  });
+  if (!tree.exists(projectJsonPath)) {
+    const projectJsonContent = {
+      "scripts": {
+        "start": "docusaurus start",
+        "build": "docusaurus build"
+      },
+      // Add other custom configurations here
+    };
+    tree.write(projectJsonPath, JSON.stringify(projectJsonContent, null, 2));
+  }
 
   // Generate additional files if needed
   generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
